28af250b69ce95cde2bdbd964204fc8c0aa242dc
Ran# <ran-n@tutanota.com>
2023-01-28 00:15:45 +0100

    moved non default argument downward

--- src/entity/Codec.py
+++ src/entity/Codec.py
@@ -3,7 +3,7 @@
 # ------------------------------------------------------------------------------
 #+ Autor:  	Ran#
 #+ Creado: 	2023/01/07 13:31:54.424384
-#+ Editado:	2023/01/27 16:45:41.302737
+#+ Editado:	2023/01/28 00:14:57.748973
 # ------------------------------------------------------------------------------
 from dataclasses import dataclass, field
 from typing import Optional, Union
@@ -15,8 +15,8 @@ from src.entity import CodecType
 class Codec:
     table_name: str = field(init=False, repr=False, default=Config().get_table_name('Codec'))
     name: str
-    name_long: Optional[str] = field(default=None)
     type_: CodecType
+    name_long: Optional[str] = field(default=None)
     desc: Optional[str] = field(default=None)
     id_: Optional[int] = field(default=None)
 
