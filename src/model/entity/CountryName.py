41cf1c21247a944c839debae24e68e8d4fb7f1d0
Ran# <ran-n@tutanota.com>
2023-02-01 21:22:36 +0100

    added internacionalization saving capability to country names

---
+++ src/model/entity/CountryName.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#+ Autor:  	Ran#
+#+ Creado: 	2023/02/01 21:18:34.961211
+#+ Editado:	2023/02/01 21:20:48.275087
+# ------------------------------------------------------------------------------
+from dataclasses import dataclass, field
+from typing import Optional, Union
+
+from src.utils import Config
+from src.model.entity import Country
+# ------------------------------------------------------------------------------
+@dataclass
+class CountryName:
+    table_name: str = field(init=False, repr=False, default=Config().get_table_name('CountryName'))
+    name: str
+    country: Country
+    id_: Optional[int] = field(default=None)
+
+    # table_name and id_ attributes are frozen
+    def __setattr__(self, attr: str, value: Union[int, str]) -> None:
+        if (attr != 'table_name'):
+            object.__setattr__(self, attr, value)
+# ------------------------------------------------------------------------------
