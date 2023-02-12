81b2cdaf059dd7c66f5cef4d6e98b15eb7c3e185
Ran# <ran-n@tutanota.com>
2023-02-12 23:02:07 +0100

    reordered and added declaration of entities

--- src/model/entity/__init__.py
+++ src/model/entity/__init__.py
@@ -3,8 +3,9 @@
 # ------------------------------------------------------------------------------
 #+ Autor:  	Ran#
 #+ Creado: 	2023/01/21 15:13:42.859161
-#+ Editado:	2023/02/12 21:05:09.340541
+#+ Editado:	2023/02/12 22:59:35.425865
 # ------------------------------------------------------------------------------
+from .Language import Language
 from .Platform import Platform
 from .Encoder import Encoder
 from .Extension import Extension
@@ -12,28 +13,17 @@ from .FolderName import FolderName
 from .WarehouseType import WarehouseType
 from .CodecType import CodecType
 from .Code import Code
-from .Language import Language
 from .MediaType import MediaType
 from .ShareSiteType import ShareSiteType
 from .MediaStatus import MediaStatus
 from .Web import Web
 from .App import App
+from .Country import Country
+
 from .AppVersion import AppVersion
-from .WarehouseTypeName import WarehouseTypeName
-from .MediaTypeName import MediaTypeName
-from .MediaStatusName import MediaStatusName
-from .MediaTypeNameLanguage import MediaTypeNameLanguage
-from .MediaStatusNameLanguage import MediaStatusNameLanguage
 from .Codec import Codec
 from .ShareSite import ShareSite
 from .Warehouse import Warehouse
-from .WarehouseName import WarehouseName
-from .WarehouseTypeNameLanguage import WarehouseTypeNameLanguage
-from .WarehouseNameLanguage import WarehouseNameLanguage
-from .LanguageName import LanguageName
-from .LanguageDescription import LanguageDescription
-from .LanguageNameLanguage import LanguageNameLanguage
-from .LanguageDescriptionLanguage import LanguageDescriptionLanguage
 from .LanguageCode import LanguageCode
 from .Media import Media
 from .MediaGroup import MediaGroup
@@ -41,13 +31,33 @@ from .MediaIssue import MediaIssue
 from .MediaPoster import MediaPoster
 from .File import File
 from .FileStream import FileStream
-from .Country import Country
-from .CountryName import CountryName
-from .CountryNameLanguage import CountryNameLanguage
-from .MediaName import MediaName
-from .MediaNameLanguage import MediaNameLanguage
-from .MediaNameCountry import MediaNameCountry
 from .ShareSiteSubs import ShareSiteSubs
 from .FileShareSite import FileShareSite
 from .MediaWeb import MediaWeb
+
+from .LanguageName import LanguageName
+#from .PlatformName import PlatformName
+from .WarehouseTypeName import WarehouseTypeName
+from .MediaTypeName import MediaTypeName
+from .MediaStatusName import MediaStatusName
+from .CountryName import CountryName
+from .WarehouseName import WarehouseName
+from .MediaName import MediaName
+
+from .LanguageNameLanguage import LanguageNameLanguage
+#from .PlatformNameLanguage import PlatformNameLanguage
+from .WarehouseTypeNameLanguage import WarehouseTypeNameLanguage
+from .MediaTypeNameLanguage import MediaTypeNameLanguage
+from .MediaStatusNameLanguage import MediaStatusNameLanguage
+from .CountryNameLanguage import CountryNameLanguage
+from .WarehouseNameLanguage import WarehouseNameLanguage
+from .MediaNameLanguage import MediaNameLanguage
+
+from .LanguageDescription import LanguageDescription
+#from .PlatformDescription import PlatformDescription
+
+from .LanguageDescriptionLanguage import LanguageDescriptionLanguage
+#from .PlatformDescriptionLanguage import PlatformDescriptionLanguage
+
+from .MediaNameCountry import MediaNameCountry
 # ------------------------------------------------------------------------------
