a89c9d8c3c3bdfa34e92e1bf4b11a900be6ab7b4
Ran# <ran-n@tutanota.com>
2023-02-23 17:30:56 +0000

    removed optional import of typing since these where copied from medias name and that has a active flag these dont

--- src/model/entity/media_status_name.py
+++ src/model/entity/media_status_name.py
@@ -3,10 +3,9 @@
 # ------------------------------------------------------------------------------
 #+ Autor:  	Ran#
 #+ Creado: 	2023/01/29 23:17:57.682598
-#+ Editado:	2023/02/19 14:13:49.019098
+#+ Editado:	2023/02/23 17:29:59.972766
 # ------------------------------------------------------------------------------
 from dataclasses import dataclass, field
-from typing import Optional
 
 from src.utils import Config
 from src.model.entity import BaseEntity, MediaStatus
--- src/model/entity/media_type_name.py
+++ src/model/entity/media_type_name.py
@@ -3,10 +3,9 @@
 # ------------------------------------------------------------------------------
 #+ Autor:  	Ran#
 #+ Creado: 	2023/01/29 23:20:04.639454
-#+ Editado:	2023/02/19 14:13:33.474052
+#+ Editado:	2023/02/23 17:30:10.201983
 # ------------------------------------------------------------------------------
 from dataclasses import dataclass, field
-from typing import Optional
 
 from src.utils import Config
 from src.model.entity import BaseEntity, MediaType
